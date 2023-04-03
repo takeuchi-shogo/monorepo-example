7b8567af45ea6728c8b1a577e4ec53142006d021
ShogoTakeuchi <smth.1.23@icloud.com>
2023-04-03 23:38:53 +0900

    Completed GraphQL connection to the frontend

---
+++ frontend/app/users/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+
+const query = gql`
+query {
+	user(id: 5) {
+		id
+		name
+	}
+}
+`
+export default function Page() {
+	const { data, loading, error } = useQuery(query)
+
+	if (loading) return <p className="text-white">Loading....</p>
+	if (error) return <p className="text-white">Error!!!</p>
+
+	return (
+		<>
+			(loading && (
+				<p className="text-white">Loading....</p>
+			))
+			(data && (
+				<div className="text-white">ユーザーページ</div>
+				<div className="text-white">{ data?.user.name }</div>
+			))
+		</>
+	)
+}
